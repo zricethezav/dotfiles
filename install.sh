07c97870240fe362c9f134ecb6b41b62deaba008
zricethezav <zricer@protonmail.com>
2020-12-21 19:07:17 -0500

    symlinks

---
+++ install.sh
@@ -0,0 +1,4 @@
+# this assumes dotfiles are cloned to ~/dotfiles
+ln -s -f ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s -f ~/dotfiles/.zshrc ~/.zshrc
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
