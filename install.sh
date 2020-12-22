2874535de0217d668c0145584f2b57f748ec577c
zricethezav <zricer@protonmail.com>
2020-12-21 21:10:09 -0500

    git configs

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-# this assumes dotfiles are cloned to ~/dotfiles
+# this assumes dotfiles are cloned to ~/dotfiles and probably
+# assumes you are using macos
 ln -s -f ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+cp .gitconfig-work ~/.gitconfig-work
+cp .gitconfig-play ~/.gitconfig-play
+ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
