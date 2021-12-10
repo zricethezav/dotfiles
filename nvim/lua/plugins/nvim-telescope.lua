ab376f34036c7762544c065d1f5723191cd6d848
zricethezav <zricer@protonmail.com>
2021-12-10 12:45:57 -0600

    switch to lua and update alacritty colors

---
+++ nvim/lua/plugins/nvim-telescope.lua
@@ -0,0 +1,8 @@
+local actions = require('telescope.actions')require('telescope').setup{
+  pickers = {
+    buffers = {
+      sort_lastused = true
+    }
+  }
+}
+
--- symlink.sh
+++ symlink.sh
@@ -3,7 +3,7 @@
 # assumes you are using macos.
 # Use the link target as directions on where to place this files.
 # This is a dumb installer so you gotta do some work.
-ln -s -f ~/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/nvim ~/.config/
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/.functions ~/.config/zsh/.functions
 ln -s -f ~/dotfiles/.alias ~/.config/zsh/.alias
