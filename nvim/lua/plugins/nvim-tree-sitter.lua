145402df65ed953eddad22285b8a2a86bc61d494
zricethezav <zricer@protonmail.com>
2022-01-12 13:28:44 -0600

    add tree-sitter highlighting

---
+++ nvim/lua/plugins/nvim-tree-sitter.lua
@@ -0,0 +1,15 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+configs.setup {
+  ensure_installed = "maintained",
+  sync_install = false,
+  ignore_instal = { "" },
+  highlight = {
+    enable = true,
+    disable = { "" },
+    additional_vim_regex_highlighting = true,
+  },
+}
